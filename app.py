b51261c1d062b740b1a4dbffcd68f3895fff9ba6
Adam <adamgirach@gmail.com>
2021-02-21 20:00:45 +0000

    Added Flask sign in page

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request
-from random import choice
 
 web_site = Flask(__name__)
 
-@web.site.route('/')
+@web_site.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
-@web_site.route('/user', methods=['POST', 'GET'])
-def login():
+@web_site.route('/user/', methods=['POST', 'GET'], defaults={'email':None, 'password':None})
+@web_site.route('/user/<email>')
+@web_site.route('/user/<password>')
+def login(email, password):
+      if not email and not password:
+        email = request.args.get('email')
+        password = request.args.get('password')
+      
+      if not email or not password:
+        return "Something went wrong"
+      
+      return "You are signed in as " + email
 
-    if request.method == 'GET':
-
-        return("<pUser: " + request.args.get('username') + "</p>")
-
-web_site.run(host='0.0.0.0', port=8080)
-
-
+#only used for repl.it:
+#web_site.run(host='0.0.0.0', port=8080)
