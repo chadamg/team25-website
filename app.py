e60d130940df9c67e168ac2d75043ce8034de604
Adam <adamgirach@gmail.com>
2021-02-22 11:04:30 +0000

    Added user.html and error page

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ web_site = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@web_site.route('/user/', methods=['POST', 'GET'], defaults={'email':None, 'password':None})
+@web_site.route('/user/', methods=['POST', 'GET'], defaults={'email': None, 'password': None})
 @web_site.route('/user/<email>')
 @web_site.route('/user/<password>')
 def login(email, password):
@@ -15,9 +15,14 @@ def login(email, password):
         password = request.args.get('password')
       
       if not email or not password:
-        return "Something went wrong"
+        return \
+          "<p>Something went wrong</p>\n" + \
+          "<a href='/'>\n" + \
+			    "<button>Home</button>\n" + \
+		      "</a>\n"
       
-      return "You are signed in as " + email
+      return render_template('user.html', email=email)
+
 
 #only used for repl.it:
 #web_site.run(host='0.0.0.0', port=8080)
